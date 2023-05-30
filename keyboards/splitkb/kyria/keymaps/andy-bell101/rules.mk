ddc1f3bd62b88ffe4e82ed6e862572ba924e01e2
Andrew Bell <andrew.bell012276@gmail.com>
2023-05-30 18:47:23 +0100

    feat(kyria): stablised kyria config
    first attempt at rgb lighting mode on screen
    
    rgb mode on oled with levels
    
    use uk keycodes
    
    key overrides
    
    tap dance
    
    remove dvorak and colemak
    
    enable caps word
    
    remove mouse accel
    
    one shot keys for shift and layers
    
    add taps for brackets
    
    remove unnecessary constant
    
    left and right enter hold for left and right alt
    
    add US QWERTY layer for mac
    
    enable tap toggle of one shot keys
    
    remove key overrides, they were annoying
    
    nav layout more vim friendly
    
    add numpad to function layer
    
    tap dance for easier _ and -
    
    remove extra } from bracket tap dance
    
    tap dance for slash and backslash
    
    formatting
    
    triple tap for double dash
    
    double tap shift for caps word
    
    switch nav and symbol keys for easier gaming
    
    fix OLED_DRIVER
    
    remove double tap underscore in favour of using - key directly
    
    add gaming layer and remove dvorak and colemak
    
    put modifier state onto oled

---
+++ keyboards/splitkb/kyria/keymaps/andy-bell101/rules.mk
@@ -0,0 +1,8 @@
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306      # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+CONVERT_TO = promicro_rp2040
+TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
